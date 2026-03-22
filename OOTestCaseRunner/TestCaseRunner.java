3249832705e2ba61ddbc7902d9aded7f73456134
USER1 <USER1@192.168.1.194>
2026-03-21 23:28:50 -0400

    added basic login test method.  Will add to it later.

--- OOTestCaseRunner/TestCaseRunner.java
+++ OOTestCaseRunner/TestCaseRunner.java
@@ -8,9 +8,10 @@ public class TestCaseRunner {
 		// TODO Auto-generated method stub
 		Scanner input = new Scanner(System.in);
 		//ArrayList<TestCase> testCases = new ArrayList<>();
-		String expectedResults = "";
+		//String expectedResults = "";
 		//String actualResults = "";
 		//String status = "";
+		String selectAnotherTest;
 		
 		boolean active = true;
 		int testCase = 0;
@@ -18,14 +19,28 @@ public class TestCaseRunner {
 			displayMenu();
 			
 			testCase = input.nextInt();
-			if (testCase == 4) active = false;
 			input.nextLine();
+			switch (testCase) {
+				case 1 : 
+					runLoginTest();
+					break;
+				default : 
+					System.out.println("Exiting Program");
+					active = false;
+			}
 			
+			System.out.println("Do you wish to conduct another test? Y or N");
+			selectAnotherTest = input.nextLine();
+			
+			if (selectAnotherTest.equals("N")) {
+				active = false;
+			}
 		}
 		
 		input.close();
 	}
 	
+	// method displays the initial menu
 	public static void displayMenu() {
 		System.out.println("Select a Test Case:");
 		System.out.println("+++++++++++++++++++");
@@ -34,6 +49,14 @@ public class TestCaseRunner {
 		System.out.println("2. Search");
 		System.out.println("3. Checkout");
 		System.out.println("4. Exit");
+	}
+	
+	// method runs the login test;
+	public static void runLoginTest() {
+		System.out.println("Login Test");
+		System.out.println("----------");
+		
+		
 	}
 
 }
